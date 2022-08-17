10e618f94ae651daa1c9f8c5d2f3ec1c80e641b6
Sviatoslav Binovskyi <santonase@gmail.com>
2022-08-17 15:02:51 +0300

    if else: practice

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,69 +1,39 @@
-//what is dictionary?
+// if else
 
-import Foundation
-var phoneBook = ["Alice": 000, "Bob": 111]
+let first = 1
+let second = 2
+let third = 3
+let fourth = 5
+let fifth = 8
+let sixth = 13
+var sum = first + second + third + fourth + fifth + sixth
+var mult = first * second * third * fourth * fifth * sixth
+sum - 10
+let newValue = mult / sum
+print(newValue)
 
-//Create empty dectionary
-let myDict1 = ["": 0]
-let muDict2 = [String(): Int()]
-let myDict3: [String: Int] = [:]
-let myDict4: Dictionary<String, Int> = [:]
 
-//Access dictionary elements
-let myElement = phoneBook["Bob"]
-//Add new elements to dictionary
+var a = 0
+var b = 2
+var c = 10
+var d = 4
 
-phoneBook["Tom"] = 333
+if a > b {
+    print("1")
+} else if a < b {
+    print("2")
+    if a + b < c {
+        print("4")
+    } else if a - b > c {
+        print("5")
+    } else if a < c || b < c {
+        print("6")
+    } else if a <= 0 {
+        print("0")
+    }
+} else {
+    print("3")
+}
 
-//Remove elements
-
-//phoneBook.removeValue(forKey: "Bob")
-//phoneBook["Alica"] = nil
-//print(phoneBook)
-
-//What if we call "nil" element?
-let myArray = [1, 2, 3, 4]
-let myNewVar = phoneBook["cc"]
-
-//Popular functions: .count, .isEmpty
-
-let countOfElements = phoneBook.count
-let isItEmpty = phoneBook.isEmpty
-
-//Access keys/values as an Array
-
-let allKeys = phoneBook.keys
-let allValues = phoneBook.values
-
-//Homework
-
-var countrys = ["Ukraine":"Kyiv", "USA":"washington", "Poland":"Warszawa"]
-var citys = ["Dnipro": 49000, "Kyiv":00000, "Kharkiv":11111]
-var isStudent: [String:Bool] = ["Bob":true, "Alice":false]
-
-countrys.removeValue(forKey: "Poland")
-citys.removeValue(forKey: "Dnipro")
-isStudent.removeValue(forKey: "Alice")
-print(countrys)
-print(citys)
-print(isStudent)
-
-var allTypes: [String: Any] = [:]
-allTypes["key1"] = 111
-allTypes["key2"] = "two"
-allTypes["secondKeysForUSA"] = countrys["USA"]
-allTypes["Dnipro"] = citys["Dnipro"]
-allTypes["Student?"] = isStudent["Bob"]
-print(allTypes)
-
-var arrayKeys = allTypes.keys
-print(arrayKeys)
-var arrayValues = citys.values
-print(arrayValues)
-
-var arrayKeys2: [Int] = [1, 2]
-arrayKeys2[0] = 2
-arrayKeys2[1] = 1
-print(arrayKeys2)
 
 
