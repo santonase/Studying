9760783cab726c88d177278d3c7eee03d705ce06
Sviatoslav Binovskyi <santonase@gmail.com>
2022-08-30 15:31:49 +0300

    Calculate BMI

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -16,7 +16,17 @@
 
 func calculateBmi(_ weight: Int, _ height: Double) -> String {
     let bmi = Double(weight) / (height * height)
+    
+    if bmi <= 18.5 {
+        return "Underweight"
+    } else if bmi <= 25.0 {
+        return "Normal"
+    } else if bmi <= 30.0 {
+        return "Overweight"
+    } else if bmi > 30 {
+        return "Obese"
+    }
     return String(bmi)
 }
 
-calculateBmi(82, 1.90)
+calculateBmi(100, 1.90)
